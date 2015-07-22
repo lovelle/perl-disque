d20cfa2889b56e314d381b167836bc3dcfbf98aa
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2015-07-22 14:15:49 -0300

    Fix in 'examples/consumer.pl' don't do unnecessary foreach

--- examples/consumer.pl
+++ examples/consumer.pl
@@ -8,13 +8,12 @@ my $disque = Disque->new(servers => ["localhost:7711", "localhost:7712"]);
 
 while (1) {
     my @jobs = $disque->get_job("test");
-    foreach (@jobs) {
-        my $queue = @{$_}[0];
-        my $job_id = @{$_}[1];
-        my $job = @{$_}[2];
-        warn "Received job: '$job' $!at queue: '$queue' $!\n";
-        $disque->ack_job($job_id);
-    }
+    my $queue = shift $jobs[0];
+    my $job_id = shift $jobs[0];
+    my $job = shift $jobs[0];
+
+    warn "Received job: '$job' $!at queue: '$queue' $!\n";
+    $disque->ack_job($job_id);
 }
 
 $disque->quit();
